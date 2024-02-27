9588d3a409bf5d1c9f87f089420ec2b919d0455d
Farbfetzen <farbfetzen@posteo.de>
2024-02-27 20:55:03 +0100

    Use SketchComponent for blue

--- src/app/sketches/blue/blue.component.ts
+++ src/app/sketches/blue/blue.component.ts
@@ -1,31 +1,23 @@
 import p5 from "p5";
 
-import { AfterViewInit, Component, OnDestroy } from "@angular/core";
+import { Component } from "@angular/core";
+
+import { SketchComponent } from "src/app/sketches/sketch/sketch.component";
 
 @Component({
     selector: "app-blue",
     standalone: true,
-    imports: [],
-    templateUrl: "./blue.component.html",
-    styleUrl: "./blue.component.css",
+    imports: [SketchComponent],
+    template: `<app-sketch [sketchFun]="createSketch" centeredHorizontally="true" />`,
 })
-export class BlueComponent implements AfterViewInit, OnDestroy {
-    sketch!: p5;
+export class BlueComponent {
     backgroundColor = "#3264c8";
-
-    ngAfterViewInit(): void {
-        this.sketch = new p5(this.createSketch);
-    }
-
-    ngOnDestroy(): void {
-        this.sketch.remove();
-    }
 
     createSketch = (p: p5) => {
         // let counter = 0;
 
         p.setup = () => {
-            p.createCanvas(300, 300).parent("canvas-container");
+            p.createCanvas(300, 300);
         };
 
         p.draw = () => {
